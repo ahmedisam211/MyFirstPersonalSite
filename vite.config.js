1c3db32307c5e2fc5f02887d09dd85f2df60e27f
Ahmed Isam <158821953+ahmedisam211@users.noreply.github.com>
2026-04-09 23:38:19 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
-    // REPLACE 'your-repo-name' with the actual name of your GitHub repository
-    //base: './MyFirstPersonalSite',
-    plugins: [react()],
+    plugins: [
+        react( ),
+    ],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
